6635eb8aee2bef49a328887ecee7b4ba745ce394
NWuSunset <136409307+NWuSunset@users.noreply.github.com>
2025-03-18 00:13:15 -0700

    Update binary_search.h

--- binary_search.h
+++ binary_search.h
@@ -1,12 +1,18 @@
 #ifndef BINARY_SEARCH_H
 #define BINARY_SEARCH_H
 
-struct Node {
-  Node* left = nullptr;
-  Node* right = nullptr;
-  Node* parent = nullptr;
-  int data;
+//found out how to label documentation
 
+struct Node {
+  Node* left = nullptr; ///< The left child
+  Node* right = nullptr; ///< The right child
+  Node* parent = nullptr; ///< Parent of node
+  int data; ///< Data node contains
+
+  /*!
+      @brief Node constructor
+      @param data the data the node will contain
+   */
   explicit Node(const int data) {
     this->data = data;
   }
@@ -16,31 +22,58 @@ struct Node {
 
 class BinaryTree {
  public:
-  
+
+  /*!
+   * @brief tree constructor
+   */
   BinaryTree();
 
-  /*
-    insert Args:
-
-    node - Node to start on
-    prev
-    data
-   */
-  
-  void insert(Node* &node, Node* prev, const int data); //insert into binary tree
+    /*!
+      @brief  Inserting data into the binary tree
+      @param  node        Pos in the tree. Should be root node to start
+      @param  prev        previous node, used in the function's recursion. Should be null to start
+      @param  data        data to be inserted into the tree
+    */
+  void insert(Node* &node, Node* prev, int data); //insert into binary tree
 
 
-  /* remove args: rem - node to remove from the tree */
+  /*!
+      @brief  removing data/node from binary tree
+      @param  rem        node to remove.
+    */
   void remove(Node* rem); //remove from binary tree
+
+  /*!
+      @brief  used in the remove function to swap a node with another
+      @param  n1        node to be removed/swapped
+      @param  n2        node to swap with
+    */
   void swapNodes(Node* n1, Node* n2);
+
+  /*!
+      @brief  Used to visualize the tree to the console
+      @param  pos       Position in the tree. Should be root to start
+      @param  depth     Depth of the tree. Should be 0 to start
+    */
+  void print(const Node* pos, int depth = 0); //Prints out the binary tree (depth starts at 0)
+
+  /*!
+        @brief  Used to visualize the tree to the console
+        @param  node     position in the tree. Should be root to start
+        @param  data     data to find
+        @returns the Node* in the tree that contains the passed in data
+        @returns nullptr if the data could not be found in the tree
+      */
   void print(const Node* pos, const int depth = 0, bool isRight = false); //Prints out the binary tree (depth starts at 0)
   Node* getNode(Node* node, int data); //checks if the tree has a specific node
-  
+
+  /*!
+   * @brief destructor
+   */
   ~BinaryTree();
 
-  Node* root = nullptr;
+  Node* root = nullptr; ///< root of the binary tree
   
-
 };
 
 #endif
